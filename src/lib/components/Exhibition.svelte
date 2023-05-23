f6d47d0140f80b7d6c849e2e213cfa20e3a4bcb2
shajwan2023 <shajwan.namiq2023@gmail.com>
2023-05-23 14:02:39 +0300

    Add exhibition page :fixing err

--- src/lib/components/Exhibition.svelte
+++ src/lib/components/Exhibition.svelte
@@ -7,15 +7,22 @@
 		created_at: Date;
 	};
 
-	console.log('test ', exhibitionSection);
+	// Convert the created_at date
+	const formattedDate = new Date(exhibitionSection.created_at).toLocaleDateString('en-US', {
+		day: 'numeric',
+		month: 'short',
+		year: 'numeric'
+	});
+
+	// console.log('test ', exhibitionSection);
 </script>
 
 <li>
 	<a href="#" class="block overflow-hidden group">
 		<img
-			src="https://images.unsplash.com/photo-1684619419581-39800cb71285?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
-			alt=""
-			class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+			src={`${import.meta.env.VITE_PUBLIC_SUPABASE_STORAGE_URL}/${exhibitionSection?.images}`}
+			alt={exhibitionSection.title}
+			class="bg-red-400 h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
 		/>
 	</a>
 	<div class="flex flex-col justify-between items-center bg-slate-950 p-10 text-white">
@@ -23,6 +30,6 @@
 		<p class="pt-5 leading-relaxed">
 			{exhibitionSection?.description}
 		</p>
-		<!-- <span class="pt-2 self-end text-sm">{exhibitionSection.created_at}</span> -->
+		<span class="pt-2 self-end text-sm"> {formattedDate}</span>
 	</div>
 </li>
--- src/routes/exhibition/+page.svelte
+++ src/routes/exhibition/+page.svelte
@@ -7,9 +7,30 @@
 	export let data;
 
 	onMount(async () => {
-		exhibitionSectionStore.get(data?.supabase);
+		await exhibitionSectionStore.get(data?.supabase);
 		console.log('data', $exhibitionSectionStore);
 	});
 </script>
 
-<h1>jk</h1>
+<svelte:head>
+	<title>Exhibition</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="py-10 dark:bg-slate-900">
+	<div class="mx-auto px-4 sm:px-5 max-w-screen-2xl">
+		<div class="flex justify-center items-center my-8">
+			<TitleUi text="Exhibition" />
+		</div>
+
+		<div class="grid gap-8 mt-8 sm:grid-cols-1 lg:grid-cols-2">
+			{#each $exhibitionSectionStore as exhibitionSection}
+				<div class="dark:bg-slate-900">
+					<ul>
+						<Exhibition {exhibitionSection} />
+					</ul>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
