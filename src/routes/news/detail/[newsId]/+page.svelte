4f468eb5aa4704b241ec09db774352c81ac9d251
Rovar Kamil <105520245+RovarQRoom@users.noreply.github.com>
2023-07-17 07:59:52 +0300

    Development (#7)
    * Fixed The Carasoul Disspering By Removing The Animation The Animation Wasn't Working With Carasoul
    
    * Fixed The Dark Mode of Contact And Layout Of News And Magazine
    
    * Updated Exhibition
    
    * update
    
    * Update

--- src/routes/news/detail/[newsId]/+page.svelte
+++ src/routes/news/detail/[newsId]/+page.svelte
@@ -30,31 +30,7 @@
 	});
 </script>
 
-<!-- <section class="dark:bg-slate-900 dark:text-white text-slate-950 flex-1 relative p-2">
-	{#if news}
-		<div class="items-start flex flex-col 3xl:flex-row justify-around">
-			<div class="w-full bg-gray-50 {Constants.page_max_width} m-auto flex-1 my-10">
-				<DetailPage imagesCarousel={news.imagesCarousel} long_description={news.long_description} />
-			</div>
-			{#if $newsStore && $newsStore.data.length > 0}
-				<RecentItems
-					title={$LL.news()}
-					items={$newsStore.data.map((news) => modelToItemModel(news))}
-					pageType={'news'}
-				/>
-			{/if}
-		</div>
-	{:else}
-		<div class="w-full h-full flex flex-1 justify-center items-center absolute">
-			<Spinner />
-		</div>
-	{/if}
-</section> 
-
-<section
-	class="dark:bg-slate-900 dark:text-white text-slate-950 {Constants.page_max_width} mx-auto w-full "
-
->
+<section class=" dark:text-white text-slate-950 {Constants.page_max_width} mx-auto w-full">
 	{#if news}
 		<div
 			class="grid 3xl:grid-cols-3 grid-cols-2 my-2 rounded-lg justify-center items-center content-center w-full"
