6910da6fb4152e008afbf74c9183730165bc9024
RovarQRoom <rovar.07000120@outlook.com>
2023-07-18 09:28:41 +0300

    Update

---
+++ src/lib/components/VideoSlider.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    //@ts-ignore
+import Carousel from 'svelte-carousel';
+import { browser } from '$app/environment';
+
+</script>
+
+{#if browser}
+<Carousel
+  let:loaded
+>
+  {#each [1,2,3] as src}
+    <div class="img-container">
+      
+    </div>
+  {/each}
+</Carousel>
+{/if}
