9ee16be5a303cb64afee3224275cd0da313d616b
RovarQRoom <rovar.07000120@outlook.com>
2023-09-14 16:04:39 +0300

    Update

--- src/lib/components/exhibitionMapImage.svelte
+++ src/lib/components/exhibitionMapImage.svelte
@@ -15,6 +15,9 @@
 	function openPdfFile(pdfLink: string) {
 		const completePdfLink = import.meta.env.VITE_PUBLIC_SUPABASE_STORAGE_PDF_URL + '/' + pdfLink;
 
+		console.log("Hello There",exhibition);
+		
+
 		const newWindow = window.open();
 		if (newWindow !== null) {
 			newWindow.document.body.innerHTML = `<iframe src="${completePdfLink}" width="100%" height="100%"></iframe>`;
