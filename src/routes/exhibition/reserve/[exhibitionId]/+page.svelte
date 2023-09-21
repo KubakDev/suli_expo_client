e5ed73e78346e1ab5bfbbf68d595744fd0f6353e
RovarQRoom <rovar.07000120@outlook.com>
2023-09-21 14:20:30 +0300

    save file

--- src/routes/exhibition/reserve/[exhibitionId]/+page.svelte
+++ src/routes/exhibition/reserve/[exhibitionId]/+page.svelte
@@ -64,71 +64,70 @@
 		console.log('reserveSeatData', exhibition.seat_layout[0].type);
 	});
 	async function reserveSeat() {
-		console.log("################################")
-		// let fileUrl = '';
-		// seatReserved = true;
-		// defaultModal = false;
-		// if (reserveSeatData.file) {
-		// 	const response = await data.supabase.storage
-		// 		.from('file')
-		// 		.upload(
-		// 			`reserve/${getRandomTextNumber()}_${reserveSeatData.file.name}`,
-		// 			reserveSeatData.file!
-		// 		);
-		// 	fileUrl = response?.data?.path;
-		// }
+		let fileUrl = '';
+		seatReserved = true;
+		defaultModal = false;
+		if (reserveSeatData.file) {
+			const response = await data.supabase.storage
+				.from('file')
+				.upload(
+					`reserve/${getRandomTextNumber()}_${reserveSeatData.file.name}`,
+					reserveSeatData.file!
+				);
+			fileUrl = response?.data?.path;
+		}
 		
 
-		// if (exhibition.seat_layout[0].type == SeatsLayoutTypeEnum.AREAFIELDS) {
-		// 	console.log('reserveSeatData', reserveSeatData);
+		if (exhibition.seat_layout[0].type == SeatsLayoutTypeEnum.AREAFIELDS) {
+			console.log('reserveSeatData', reserveSeatData);
 			
-		// 	data.supabase
-		// 		.from('seat_reservation')
-		// 		.insert({
-		// 			exhibition_id: exhibition.id,
-		// 			company_id: $currentUser.id,
-		// 			object_id: new Date().getTime(),
-		// 			comment: reserveSeatData.comment,
-		// 			reserved_areas: JSON.stringify(reserveSeatData.area),
-		// 			status: ReservationStatusEnum.PENDING,
-		// 			type: exhibition.seat_layout[0].type,
-		// 			file_url: fileUrl
-		// 		})
-		// 		.then(() => {
-		// 			selectedSeat.set(null);
-		// 			setTimeout(() => {
-		// 				goto('/exhibition/1');
-		// 			}, 3000);
-		// 			fetch('/api/seat/purchase', {
-		// 				method: 'POST',
-		// 				headers: {
-		// 					'Content-Type': 'application/json'
-		// 				},
-		// 				body: JSON.stringify({
-		// 					emailUser: data?.session?.user?.email,
-		// 					name: '',
-		// 					message: '',
-		// 					exhibition: exhibition,
-		// 					companyData: $currentUser,
-		// 					reserveSeatData: reserveSeatData
-		// 				})
-		// 			}).then(() => {
-		// 				console.log('email sent');
+			data.supabase
+				.from('seat_reservation')
+				.insert({
+					exhibition_id: exhibition.id,
+					company_id: $currentUser.id,
+					object_id: new Date().getTime(),
+					comment: reserveSeatData.comment,
+					reserved_areas: JSON.stringify(reserveSeatData.area),
+					status: ReservationStatusEnum.PENDING,
+					type: exhibition.seat_layout[0].type,
+					file_url: fileUrl
+				})
+				.then(() => {
+					selectedSeat.set(null);
+					setTimeout(() => {
+						goto('/exhibition/1');
+					}, 3000);
+					fetch('/api/seat/purchase', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							emailUser: data?.session?.user?.email,
+							name: '',
+							message: '',
+							exhibition: exhibition,
+							companyData: $currentUser,
+							reserveSeatData: reserveSeatData
+						})
+					}).then(() => {
+						console.log('email sent');
 						
-		// 			});
-		// 			defaultModal = true;
-		// 		});
-		// } else {
-		// 	data.supabase
-		// 		.from('seat_reservation')
-		// 		.insert(reserveSeatData)
-		// 		.then(() => {
-		// 			selectedSeat.set(null);
-		// 			setTimeout(() => {
-		// 				goto('/exhibition/1');
-		// 			}, 3000);
-		// 		});
-		// }
+					});
+					defaultModal = true;
+				});
+		} else {
+			data.supabase
+				.from('seat_reservation')
+				.insert(reserveSeatData)
+				.then(() => {
+					selectedSeat.set(null);
+					setTimeout(() => {
+						goto('/exhibition/1');
+					}, 3000);
+				});
+		}
 	}
 	onDestroy(() => {
 		localStorage.removeItem('redirect');
