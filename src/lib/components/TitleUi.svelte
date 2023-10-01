023f0b344b6c50ab950ee28a9c8709966f8a7257
bnar98 <bnar.paskandy@gmail.com>
2023-10-01 07:49:26 +0300

    redesign all color based elements , all depend on current active design that admin selected

--- src/lib/components/TitleUi.svelte
+++ src/lib/components/TitleUi.svelte
@@ -1,32 +1,19 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { themeToggle } from '../../stores/darkMode';
-	import { getNameRegex } from '../../utils/urlRegexName';
-
 	export let text: string = 'Title Here';
 	export let footerSize: boolean = false;
-	const routeRegex = /\/(news|exhibition|gallery|magazine|publishing|video)/;
-	let tailVar: string = 'light';
-
-	$: {
-		if (routeRegex.test($page.url.pathname)) {
-			let pageName = getNameRegex($page.url.pathname);
-			tailVar = $themeToggle === 'light' ? pageName + 'Light' : pageName + 'Dark';
-		} else {
-			tailVar = $themeToggle === 'light' ? 'light' : 'dark';
-		}
-	}
+	export let borderColor: string = 'primary-500';
+	export let textColor: string = 'var(--{tailVar}OverlaySecondaryColor)';
 </script>
 
 <div>
-	<div class="h-[1px] bg-primary-500" />
+	<div class="h-[1px]" style="background-color: {borderColor};" />
 	<h1
-		style="color:var(--{tailVar}OverlaySecondaryColor)"
+		style="color:{textColor}"
 		class="{footerSize
 			? ' font-sans'
 			: ' sm:text-4xl font-bold'} uppercase text-center my-1 px-4 ms:px-10 text-2xl"
 	>
 		{text}
 	</h1>
-	<div class="h-[1px] bg-primary-500" />
+	<div class="h-[1px]" style="background-color: {borderColor};" />
 </div>
