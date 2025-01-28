0c98ab6aebe94448ffc6f053dd003fad9ba3a587
sulyExpo <suliexpodev@gmail.com>
2025-01-28 15:54:41 +0300

    switch between toggle and languagedropdown

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -455,6 +455,170 @@
 
       <!-- Right Section: Profile and Theme Toggle -->
       <div class="flex items-center space-x-4">
+       <!-- Profile Dropdown -->
+       {#if $currentUser && $currentUser.id}
+       <div class="relative" bind:this={profileDropdown}>
+         <button
+           style="background-color: {navbarStyles.backgroundColor}; color: {navbarStyles.color};"
+           on:click={toggleDropdownProfile}
+           class="flex items-center px-3 py-2 text-sm font-medium rounded-md focus:outline-none"
+           aria-haspopup="true"
+           aria-expanded={dropdownOpenProfile}
+         >
+           <img
+             src={`${import.meta.env.VITE_PUBLIC_SUPABASE_STORAGE_URL}/${$currentUser?.logo_url}`}
+             alt="User Avatar"
+             class="w-8 h-8 rounded-full mx-2"
+           />
+           <span class="hidden sm:inline">{$currentUser.company_name}</span>
+           {#if notifications.length > 0}
+             <span class="ml-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+               {notifications.length}
+             </span>
+           {/if}
+           <ChevronDown class="w-4 h-4 ml-1" />
+         </button>
+
+            {#if dropdownOpenProfile}
+     <div
+       class="absolute {dropdownPosition} mt-2 {dropdownWidth} rounded-lg shadow-lg z-50 overflow-hidden"
+       style="background-color: {navbarStyles.backgroundColor}; color: {navbarStyles.color}; top: 100%; max-width: calc(100vw - 2rem); transition: all 0.3s ease-in-out; {maxDropdownHeight};" 
+       on:click|stopPropagation
+       on:keydown|stopPropagation
+       role="menu"
+       tabindex="0"
+     >
+       <div class="flex flex-col h-full max-h-60 overflow-y-auto">  
+         <div class="flex-grow overflow-y-auto custom-scrollbar">
+           <ul class="py-2 px-1">
+                     <li>
+                       <button
+                         class="w-full text-left px-3 py-2 text-sm rounded-md"
+                         on:click={() => {
+                           goto(`/exhibition/reserve/register/${$currentUser.uid}`);
+                           dropdownOpenProfile = false;
+                         }}
+                       >
+                         <div class="flex items-center">
+                           <UserSolid class="h-4 w-4 mr-2" />
+                           {$LL.profile.title()}
+                         </div>
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         class="w-full text-left px-3 py-2 text-sm rounded-md"
+                         on:click={() => {
+                           goto('/reservation_history');
+                           dropdownOpenProfile = false;
+                         }}
+                       >
+                         <div class="flex items-center">
+                           <svg
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                             stroke-width="1.5"
+                             stroke="currentColor"
+                             class="w-5 h-5 mr-2"
+                           >
+                             <path
+                               stroke-linecap="round"
+                               stroke-linejoin="round"
+                               d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
+                             />
+                           </svg>
+                           {$LL.profile.reservation_history()}
+                         </div>
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         class="w-full text-left px-3 py-2 text-sm rounded-md"
+                         on:click={logoutFunction}
+                       >
+                         <div class="flex items-center">
+                           <svg
+                             xmlns="http://www.w3.org/2000/svg"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                             stroke-width="1.5"
+                             stroke="currentColor"
+                             class="w-5 h-5 mr-2"
+                           >
+                             <path
+                               stroke-linecap="round"
+                               stroke-linejoin="round"
+                               d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                             />
+                           </svg>
+                           {$LL.profile.logout()}
+                         </div>
+                       </button>
+                     </li>
+                     {#if notifications.length > 0}
+                       <div class="border-t mt-2">
+                         <button
+                           class="w-full text-left px-3 py-2 text-sm rounded-md"
+                           on:click={() => {
+                             goto('/reservation_history');
+                             dropdownOpenProfile = false;
+                           }}
+                         >
+                           <div class="flex items-center">
+                             <svg
+                               xmlns="http://www.w3.org/2000/svg"
+                               fill="none"
+                               viewBox="0 0 24 24"
+                               stroke-width="1.5"
+                               stroke="currentColor"
+                               class="w-4 h-4 mr-2"
+                             >
+                               <path
+                                 stroke-linecap="round"
+                                 stroke-linejoin="round"
+                                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                               />
+                             </svg>
+                             <span class="truncate">{$LL.profile.reservation_notification()}</span>
+                             <span class="ml-auto text-red-600 font-bold">{notifications.length}</span>
+                           </div>
+                         </button>
+                       </div>
+                       <ul class="py-2 px-1">
+                         {#each notifications as notificationData}
+                           <li 
+                             class="px-3 py-2 text-sm hover:bg-opacity-10 hover:bg-gray-200 dark:hover:bg-opacity-10 dark:hover:bg-gray-700 transition-colors duration-200 rounded-md cursor-pointer"
+                             on:click={async () => {
+                               // Mark as seen first
+                               await markNotificationAsSeen(notificationData.id);
+                               // Then navigate and close dropdown
+                               goto('/reservation_history');
+                               dropdownOpenProfile = false;
+                             }}
+                           >
+                             <div class="flex justify-between items-center">
+                               <span class="truncate flex-grow mr-2">{notificationData.exhibition_name}</span>
+                               <span
+                                 class={`${
+                                   notificationData.status === 'accept' ? 'bg-green-500' : 'bg-red-500'
+                                 } w-2 h-2 rounded-full flex-shrink-0`}
+                               ></span>
+                             </div>
+                             <p class="mt-1 text-xs text-opacity-80 truncate">{notificationData.message ?? ''}</p>
+                           </li>
+                         {/each}
+                       </ul>
+                     {/if}
+                   </ul>
+                 </div>
+               </div>
+             </div>
+           {/if}
+         </div>
+       {/if}
+      
+      
         <!-- Theme Toggle -->
         <button
           on:click={toggleThemeHandler}
@@ -470,169 +634,7 @@
           {/if}
         </button>
 
-        <!-- Profile Dropdown -->
-        {#if $currentUser && $currentUser.id}
-        <div class="relative" bind:this={profileDropdown}>
-          <button
-            style="background-color: {navbarStyles.backgroundColor}; color: {navbarStyles.color};"
-            on:click={toggleDropdownProfile}
-            class="flex items-center px-3 py-2 text-sm font-medium rounded-md focus:outline-none"
-            aria-haspopup="true"
-            aria-expanded={dropdownOpenProfile}
-          >
-            <img
-              src={`${import.meta.env.VITE_PUBLIC_SUPABASE_STORAGE_URL}/${$currentUser?.logo_url}`}
-              alt="User Avatar"
-              class="w-8 h-8 rounded-full mx-2"
-            />
-            <span class="hidden sm:inline">{$currentUser.company_name}</span>
-            {#if notifications.length > 0}
-              <span class="ml-1 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
-                {notifications.length}
-              </span>
-            {/if}
-            <ChevronDown class="w-4 h-4 ml-1" />
-          </button>
-
-             {#if dropdownOpenProfile}
-      <div
-        class="absolute {dropdownPosition} mt-2 {dropdownWidth} rounded-lg shadow-lg z-50 overflow-hidden"
-        style="background-color: {navbarStyles.backgroundColor}; color: {navbarStyles.color}; top: 100%; max-width: calc(100vw - 2rem); transition: all 0.3s ease-in-out; {maxDropdownHeight};" 
-        on:click|stopPropagation
-        on:keydown|stopPropagation
-        role="menu"
-        tabindex="0"
-      >
-        <div class="flex flex-col h-full max-h-60 overflow-y-auto">  
-          <div class="flex-grow overflow-y-auto custom-scrollbar">
-            <ul class="py-2 px-1">
-                      <li>
-                        <button
-                          class="w-full text-left px-3 py-2 text-sm rounded-md"
-                          on:click={() => {
-                            goto(`/exhibition/reserve/register/${$currentUser.uid}`);
-                            dropdownOpenProfile = false;
-                          }}
-                        >
-                          <div class="flex items-center">
-                            <UserSolid class="h-4 w-4 mr-2" />
-                            {$LL.profile.title()}
-                          </div>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          class="w-full text-left px-3 py-2 text-sm rounded-md"
-                          on:click={() => {
-                            goto('/reservation_history');
-                            dropdownOpenProfile = false;
-                          }}
-                        >
-                          <div class="flex items-center">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              class="w-5 h-5 mr-2"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
-                              />
-                            </svg>
-                            {$LL.profile.reservation_history()}
-                          </div>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          class="w-full text-left px-3 py-2 text-sm rounded-md"
-                          on:click={logoutFunction}
-                        >
-                          <div class="flex items-center">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              class="w-5 h-5 mr-2"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-                              />
-                            </svg>
-                            {$LL.profile.logout()}
-                          </div>
-                        </button>
-                      </li>
-                      {#if notifications.length > 0}
-                        <div class="border-t mt-2">
-                          <button
-                            class="w-full text-left px-3 py-2 text-sm rounded-md"
-                            on:click={() => {
-                              goto('/reservation_history');
-                              dropdownOpenProfile = false;
-                            }}
-                          >
-                            <div class="flex items-center">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-4 h-4 mr-2"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                                />
-                              </svg>
-                              <span class="truncate">{$LL.profile.reservation_notification()}</span>
-                              <span class="ml-auto text-red-600 font-bold">{notifications.length}</span>
-                            </div>
-                          </button>
-                        </div>
-                        <ul class="py-2 px-1">
-                          {#each notifications as notificationData}
-                            <li 
-                              class="px-3 py-2 text-sm hover:bg-opacity-10 hover:bg-gray-200 dark:hover:bg-opacity-10 dark:hover:bg-gray-700 transition-colors duration-200 rounded-md cursor-pointer"
-                              on:click={async () => {
-                                // Mark as seen first
-                                await markNotificationAsSeen(notificationData.id);
-                                // Then navigate and close dropdown
-                                goto('/reservation_history');
-                                dropdownOpenProfile = false;
-                              }}
-                            >
-                              <div class="flex justify-between items-center">
-                                <span class="truncate flex-grow mr-2">{notificationData.exhibition_name}</span>
-                                <span
-                                  class={`${
-                                    notificationData.status === 'accept' ? 'bg-green-500' : 'bg-red-500'
-                                  } w-2 h-2 rounded-full flex-shrink-0`}
-                                ></span>
-                              </div>
-                              <p class="mt-1 text-xs text-opacity-80 truncate">{notificationData.message ?? ''}</p>
-                            </li>
-                          {/each}
-                        </ul>
-                      {/if}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            {/if}
-          </div>
-        {/if}
-      </div>
+       </div>
     </div>
   </div>
 
